dfc9b4f18fc82eea91bef164db0a43dd3617ce69
MRcoin <szymciokulec2003@gmail.com>
2022-09-04 22:24:00 +0200

    updated preview generation

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from PIL import Image
 
 
 
+
+
 st.set_page_config(
     page_title="Minecraft Skin App", page_icon=Image.open('icon.png'), initial_sidebar_state="expanded"
 )
@@ -19,37 +21,39 @@ st.write(
 """
 )
 
-async def preview_updater(test):
-    angle = 0
-    while True:
-        angle += 1
+with st.expander("How to use.", expanded= True):
+    st.write("""
+             The chart above shows some numbers I picked for you.
+             I rolled actual dice for these, so they're *guaranteed* to
+             be random.
+         """)
+    st.image("https://static.streamlit.io/examples/dice.jpg")
 
-        if angle > 71:
-            angle = 0
-        test.image(preview[angle])
-
-        await asyncio.sleep(0.08)
 
 
 image = st.file_uploader('Upload an image from which you want to generate a skin.', type=['png', 'jpg', 'jpeg'], accept_multiple_files=False)
 
 col1, col2 = st.columns(2)
-
+skin = None
 with col1:
-    generate_preview = st.button('Preview')
+    st.write()
+    generate_preview = st.button('Generate And Preview', disabled=image is None)
 
 with col2:
     if image is not None:
         if generate_preview:
             skin = skin_maker.make_skin(image)
-            hr = 30
-            preview = []
-            for angle in range(72):
-                preview.append(asyncio.run(skin_render.render(skin, hr=hr)))
-                hr -= 5
-            gif_preview = BytesIO()
-            preview[0].save(gif_preview,format='GIF',save_all=True, append_images=preview[1:], disposal=2, loop=0)
-            st.image(gif_preview)
-            # test = st.image('preview.apng')
-            # asyncio.run(preview_updater(test))
+            preview_gif = skin_render.create_preview(skin)
+            st.image(preview_gif)
             generate_preview = not generate_preview
+col3, col4, col5 = st.columns([1,1,4])
+with col3:
+    if skin is not None:
+        output_skin = BytesIO()
+        skin.save(output_skin,format='PNG')
+        downloaded = st.download_button('Download', file_name='skin.png', data=output_skin)
+    else:
+        st.button('Download', disabled=True)
+with col4:
+    if st.button('Donate ❤',key='balls'):
+        st.markdown('<meta http-equiv="refresh" content="0;url=https://donate.stripe.com/test_4gw3cqcdCfoEa9W9AA">',unsafe_allow_html=True)
--- skin_render.py
+++ skin_render.py
@@ -1,3 +1,6 @@
+import asyncio
+from io import BytesIO
+
 import MinePI
 
 from PIL import Image
@@ -8,3 +11,14 @@ async def render(image, vr=30, hr=30):
     out = Image.new(mode="RGBA", size=(250, 400), color=(0,0,0,0))
     out.paste(im,(int((250-im.size[0])/2),int((400-im.size[1])/2)))
     return out
+
+
+def create_preview(skin):
+    hr = 30
+    preview = []
+    for angle in range(72):
+        preview.append(asyncio.run(render(skin, hr=hr)))
+        hr -= 5
+    gif_preview = BytesIO()
+    preview[0].save(gif_preview, format='GIF', save_all=True, append_images=preview[1:], disposal=2, loop=0)
+    return gif_preview
