1e0500fd3f82e6a61bb23b3987ed4e6054322d8f
MRcoin <szymciokulec2003@gmail.com>
2022-09-04 20:06:31 +0200

    updated preview generation

--- skin_render.py
+++ skin_render.py
@@ -1,5 +1,10 @@
 import MinePI
 
+from PIL import Image
+
+
 async def render(image, vr=30, hr=30):
     im = await MinePI.render_3d_skin(skin_image=image, vr=-vr, hr=hr)
-    return im
+    out = Image.new(mode="RGBA", size=(250, 400), color=(0,0,0,0))
+    out.paste(im,(int((250-im.size[0])/2),int((400-im.size[1])/2)))
+    return out
