7addc85511f332f5ad193a1f993ef16bd14f6e2d
MRcoin <szymciokulec2003@gmail.com>
2022-09-04 19:48:36 +0200

    trying stuff

--- skin_render.py
+++ skin_render.py
@@ -1,12 +1,5 @@
 import MinePI
-import asyncio
-from PIL import Image, ImageTk
 
 async def render(image, vr=30, hr=30):
-    #Render a full body skin
-    im = await MinePI.render_3d_skin(skin_image=image, vr=-vr, hr=hr)#, vrll=10, vrrl=-10, vrla=-10, vrra=10
-    #
-    # im.show()
-    # im = ImageTk.getimage(im)
+    im = await MinePI.render_3d_skin(skin_image=image, vr=-vr, hr=hr)
     return im
-# asyncio.run(render())
