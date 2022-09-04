0ec42426ca90cd5cd40e58a9a3f94d8c19165a57
MRcoin <szymciokulec2003@gmail.com>
2022-09-04 16:18:23 +0200

    trying stuff

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-import scipy.stats
-from scipy.stats import norm
-import altair as alt
 
 st.set_page_config(
     page_title="A/B Testing App", page_icon="📊", initial_sidebar_state="expanded"
